55e158117e07cd5cb1b6db8c7bea45aee249af4c
gaurishanker <gaurishankargaur93@gmail.com>
2017-06-15 23:54:16 +0530

    python for informatics exercises

---
+++ Python for informatics Exercise/Chapter2 Exercise.py
@@ -0,0 +1,22 @@
+# Ex 1
+name = raw_input("Enter Your Name\n")
+print name
+
+# Ex 2
+hours = int(raw_input("Enter Hours\n"))
+rate = float(raw_input("Enter Rate\n"))
+pay = hours * rate
+print pay
+
+# Ex 3
+width = 17
+height=12.0
+print width/2		# 8
+print width/2.0		# 8.5
+print height/2		# 6.0
+print 1 + 2*5		# 11
+
+# Ex 4
+celcius = float(raw_input("Enter Temperature in Celcius\n"))
+farenhiet = celcius * 9/5 + 32
+print farenhiet
